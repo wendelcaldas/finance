543d6ae18a910fcbedd74ff91c7d302b8eadaa01
tiecvitoria <desenvolvimentoti@ecvitoria.com.br>
2025-03-13 15:57:51 -0300

    primeiro commit

--- app/Http/Controllers/FinanceiroController.php
+++ app/Http/Controllers/FinanceiroController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class FinanceiroController extends Controller
 {
     //
-    public function index(){
+    public function fluxoDeCaixa(){
         return 'teste';
     }
 }
